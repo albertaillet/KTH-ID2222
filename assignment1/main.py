3e718f83ffd82d9844e5457f6f1ed73e51097bf7
Albert Sund Aillet <albert@sundaillet.com>
2022-11-03 15:31:55 +0100

    added shingle and Jaccard distance

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,6 +1,8 @@
 # %% Imports
 import pandas as pd
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
 from nltk.corpus import reuters
 
 # %% Create Spark Session
@@ -12,7 +14,40 @@ documents = reuters.fileids()
 text = [reuters.raw(doc) for doc in documents]
 
 # %% Create a dataframe with the documents
-df = pd.DataFrame({'document': documents, 'text': text})
+pandas_df = pd.DataFrame({'document': documents, 'text': text})
 
 # %%
-df = spark.createDataFrame(df)
+df = spark.createDataFrame(pandas_df)
+
+# %%
+k = 10
+@F.udf(returnType=ArrayType(StringType()))
+def shingle(text: str):
+    return list(set(hash(text[i : i + k]) for i in range(len(text) - k + 1)))
+
+# %%
+df = df.withColumn('shingles', shingle('text'))
+
+# %%
+udf_len = F.udf(len, IntegerType())
+df = df.withColumn('n_shingles', udf_len('shingles'))
+
+# %%
+udf_count = F.udf(len, IntegerType())
+df = df.withColumn('text_length', udf_count('text'))
+
+# %%
+class Jaccard:
+    @staticmethod
+    @F.udf(returnType=FloatType())
+    def distance(list1, list2):
+        set1, set2 = set(list1), set(list2)
+        intersection = set1 & set2
+        union = set1 | set2
+        return len(intersection) / len(union)
+
+
+# %%
+df = df.withColumn('jaccard', Jaccard.distance('shingles', 'shingles'))
+
+# %%
