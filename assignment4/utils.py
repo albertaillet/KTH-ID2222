68a4b02fc7fe4f85a37497603840170d9346a601
Albert Sund Aillet <albert@sundaillet.com>
2022-12-05 09:00:09 +0100

    formatted utils

--- assignment4/utils.py
+++ assignment4/utils.py
@@ -1,11 +1,10 @@
-# imports 
+# imports
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
-import networkx as nx
 from sklearn.cluster import KMeans
 
-# typing 
+# typing
 from numpy import ndarray
 from typing import Optional
 
@@ -15,10 +14,10 @@ def load_data(filename: str) -> list[tuple[int, int]]:
     with open(f'data/{filename}.dat', newline='') as csvfile:
         edges_reader = list(csv.reader(csvfile, delimiter=','))
         values = len(edges_reader[0])
-        if values==2:
-            edges = list(tuple((int(a), int(b))) for a, b in edges_reader)
+        if values == 2:
+            edges = [(int(a), int(b)) for a, b in edges_reader]
         else:
-            edges = list(tuple((int(a), int(b))) for a, b, c in edges_reader)
+            edges = [(int(a), int(b)) for a, b, c in edges_reader]
     return edges
 
 
@@ -26,8 +25,8 @@ def get_adj_mtx(edges_list: list[tuple[int, int]]) -> ndarray:
     max_idx = max(max(*edges_list))
     A = np.zeros(shape=(max_idx, max_idx), dtype=int)
     for i, j in edges_list:
-        A[i-1, j-1] = 1
-        A[j-1, i-1] = 1
+        A[i - 1, j - 1] = 1
+        A[j - 1, i - 1] = 1
     return A
 
 
@@ -38,21 +37,21 @@ def get_laplacian(A: ndarray) -> ndarray:
     return L
 
 
-def spectral_clustering(L: ndarray, k: Optional[int]=None) -> ndarray:
+def spectral_clustering(L: ndarray, k: Optional[int] = None) -> ndarray:
 
     # get eigenvalues and eigenvectors
     eigenvalues, eigenvectors = np.linalg.eigh(L)
     # compute the number of eigenvectors k
     if k is None:
-        k = int(len(eigenvalues) - np.argmax(np.diff(eigenvalues))) 
+        k = int(len(eigenvalues) - np.argmax(np.diff(eigenvalues)))
         print(f'Number of clusters using the heuristic: {k}')
- 
+
     # exctract the k biggest eigenvectors in X
     X = eigenvectors[:, -k:]
-    
+
     # renormalize X rows to get Y
-    Y = X / np.power(np.sum(X**2, axis=1, keepdims=True), 0.5)
-    
+    Y = X / np.sqrt(np.sum(X**2, axis=1, keepdims=True))
+
     # cluster the rows of Y using k-means clustering
     kmeans = KMeans(n_clusters=k).fit(Y)
     labels = kmeans.labels_
@@ -60,16 +59,17 @@ def spectral_clustering(L: ndarray, k: Optional[int]=None) -> ndarray:
 
 
 def draw_graph(A: ndarray, labels: ndarray, ax: plt.Axes) -> None:
+    import networkx as nx
+
     graph = nx.from_numpy_array(A)
     pos = nx.spring_layout(graph)
     ax.set_title('Clusters')
     nx.draw_networkx(graph, pos=pos, node_size=10, node_color=labels, with_labels=False, cmap=plt.cm.Set3, ax=ax)  # type: ignore
-    # plt.show()
 
 
 def plot_fiedler(L: ndarray, ax) -> None:
     # get eigenvalues and eigenvectors
-    w, eigenvectors = np.linalg.eigh(L)
+    _, eigenvectors = np.linalg.eigh(L)
 
     # extract the Fiedler vector (second smallest eigenvector)
     fiedler_eigenvector = eigenvectors[:, 1]
@@ -77,7 +77,6 @@ def plot_fiedler(L: ndarray, ax) -> None:
     ax.plot(np.arange(len(fiedler_eigenvector)), sorted(fiedler_eigenvector))
     ax.set_title('Fiedler vector')
     ax.grid()
-    # plt.show()
 
 
 def plot_eigengap(L: ndarray, ax) -> None:
@@ -88,17 +87,15 @@ def plot_eigengap(L: ndarray, ax) -> None:
     ax.plot(np.arange(len(w) - 1), sorted(np.diff(w)), 'o')
     ax.set_title('Eigengap')
     ax.grid()
-    # plt.show()
 
 
 def plot_sparsity_pattern(L: ndarray, ax) -> None:
     ax.set_title('Sparsity pattern of the Laplacian matrix')
     ax.spy(L)
-    # plt.show()
 
 
-def plots(L: ndarray, A:ndarray, labels: ndarray, k: Optional[int]=None) -> None:
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+def plots(L: ndarray, A: ndarray, labels: ndarray, k: Optional[int] = None) -> None:
+    _, axs = plt.subplots(2, 2, figsize=(10, 10))
     plot_sparsity_pattern(L, axs[0, 0])
     plot_eigengap(L, axs[0, 1])
     plot_fiedler(L, axs[1, 0])
